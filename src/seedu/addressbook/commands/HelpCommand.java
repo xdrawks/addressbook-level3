55d942a1338f1cec9bec2c97e0db5d08e6d0a117
Xien Dong Wang <xdrawks@gmail.com>
2016-09-13 20:43:15 +0800

    Implement abstract method in HelpCommand

--- src/seedu/addressbook/commands/HelpCommand.java
+++ src/seedu/addressbook/commands/HelpCommand.java
@@ -27,4 +27,10 @@ public class HelpCommand extends Command {
     public CommandResult execute() {
         return new CommandResult(MESSAGE_ALL_USAGES);
     }
+
+    @Override
+    public boolean isMutating() {
+        // Help is not mutable
+        return false;
+    }
 }
