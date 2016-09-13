b112bc97bace9fdefbb4ce6b7b2bf1c503d6fd0b
Xien Dong Wang <xdrawks@gmail.com>
2016-09-13 20:43:07 +0800

    Implement abstract method in FindCommand

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -53,4 +53,10 @@ public class FindCommand extends Command {
         return matchedPersons;
     }
 
+    @Override
+    public boolean isMutating() {
+        // Find is not mutable.
+        return false;
+    }
+
 }
