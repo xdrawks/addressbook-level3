66851f673658e38d03b1a7b53547d6b60bbd8152
Xien Dong Wang <xdrawks@gmail.com>
2016-09-13 20:42:23 +0800

    Implement abstract method in ClearCommand

--- src/seedu/addressbook/commands/ClearCommand.java
+++ src/seedu/addressbook/commands/ClearCommand.java
@@ -19,4 +19,11 @@ public class ClearCommand extends Command {
         addressBook.clear();
         return new CommandResult(MESSAGE_SUCCESS);
     }
+
+
+    @Override
+    public boolean isMutating() {
+        // Clear is mutable
+        return true;
+    }
 }
