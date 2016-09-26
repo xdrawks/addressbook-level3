95df87c4549bb3bbac06b82fae1d58ce67540c7c
Xien Dong Wang <xdrawks@gmail.com>
2016-09-26 22:24:07 +0800

    Added StorageStub that implements the Storage interface, and added javadocs

---
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -0,0 +1,29 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+/**
+ * Represents a dummy file used for LogicTest.
+ */
+public class StorageStub implements Storage {
+
+    @Override
+    public void save(AddressBook addressBook) throws StorageOperationException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public AddressBook load() throws StorageOperationException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getPath() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
