173f04121279c811042f4837e2085c0cc5b7df13
Xien Dong Wang <xdrawks@gmail.com>
2016-09-26 22:59:24 +0800

    Added a constructor to StorageStub to save reference to the AddressBook

--- src/seedu/addressbook/storage/StorageStub.java
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -10,6 +10,13 @@ public class StorageStub implements Storage {
     
     //Keep a shadow copy of the address book.
     private AddressBook addressBook;
+    
+    /**
+     * Constructs a StorageStub with a reference to the address book that the implementing class uses.
+     */
+    public StorageStub (AddressBook addressBook) {
+        this.addressBook = addressBook;
+    }
 
     @Override
     public void save(AddressBook addressBook) throws StorageOperationException {
